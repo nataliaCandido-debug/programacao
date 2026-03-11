c4267787726af603c3788f68809ede0e1bafd4f7
nataliaCandido-debug <natalia_vatrin@estudante.sesisenai.org.br>
2026-03-11 14:20:05 +0000

    update

---
+++ atividades python/LISTA 1 Python/desafios python/DESAFIO19.PY
@@ -0,0 +1,3 @@
+quantidade_frangos = int(input("Digite a quantidade de frangos a serem identificado: "))
+custo_total = (quantidade_frangos * 4.00) + (quantidade_frangos * 7.00)
+print(f"O custo total para identificar {quantidade_frangos} frangos é de R$ {custo_total:.2f}")
