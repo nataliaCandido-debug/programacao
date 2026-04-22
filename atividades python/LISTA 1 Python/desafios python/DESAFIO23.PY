3f82221635b283093da0c5ebc32f5bf23301b066
nataliaCandido-debug <natalia_vatrin@estudante.sesisenai.org.br>
2026-04-22 14:14:53 +0000

    update

---
+++ atividades python/LISTA 1 Python/desafios python/DESAFIO23.PY
@@ -0,0 +1,5 @@
+altura_pessoa = float(input("digite altura"))
+sombra_pessoa = float(input("digite o comprimento da sua sombra"))
+sombra_predio = float(input("digite a sombra do predio"))
+altura_predio = (altura_pessoa/sombra_predio) sombra_predio
+print("altura do predio eh", altura_predio)
